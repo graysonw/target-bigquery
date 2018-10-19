d33194b9fcddf6cad465c44406e16f180146c027
Paul Tiplady <paul@qwil.co>
2018-10-19 13:48:04 -0700

    Fix setup.py to allow recent singer-python versions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='target-bigquery',
       py_modules=['target_bigquery'],
       install_requires=[
           'jsonschema==2.6.0',
-          'singer-python==1.5.0',
+          'singer-python>=1.5.0',
           'google-api-python-client==1.6.2',
           'google-cloud==0.32.0'
       ],
